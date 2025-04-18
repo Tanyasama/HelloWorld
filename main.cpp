c346aad16b538826044b9cb5c5dae617cb673bb3
Tanya <2734775341@qq.com>
2025-04-18 11:02:21 +0800

    local branch test

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     cout << "hello world" << endl;
-    cout << "git pull" << endl;
+    cout << "local pull" << endl;
     cout << "new branch test" << endl;
     return 0;
 }
